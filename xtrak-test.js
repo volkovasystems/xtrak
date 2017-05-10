1b7743f09fc03bf79b4179f13229681624f22a1e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-10 15:08:44 +0800

    Updated test.

--- xtrak-test.js
+++ xtrak-test.js
@@ -1,4 +1,7 @@
-
+const assert = require( "assert" );
 const xtrak = require( "./xtrak.js" );
 
-console.log( xtrak( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ], NUMBER ) );
+assert.deepEqual( xtrak( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ], NUMBER ),
+[ 1, 2, 3, 5, 6 ] );
+
+console.log( "ok" );
